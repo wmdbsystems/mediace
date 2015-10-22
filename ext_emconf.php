6dfdb8bfca2919808d2c6e08eb94a318cf2c86e6
TYPO3 Extension Repository (TER) <git@wmdb.de>
2015-10-22 21:30:23 +0200

    Initial import of mediace

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "mediace".
+ *
+ * Auto generated 22-10-2015 21:30
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Media Content Element',
+	'description' => 'The media functionality from TYPO3 6.2 and earlier can be found here. This extension provides ContentObjects and Content Elements.',
+	'category' => 'fe',
+	'shy' => 1,
+	'version' => '7.6.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '7.5.0-7.9.99',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => 'uploads/media',
+	'modify_tables' => '',
+	'clearcacheonload' => '',
+	'lockType' => '',
+	'author' => 'TYPO3 Community - Friends of TYPO3',
+	'author_email' => 'friendsof@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
